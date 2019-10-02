4e40610464575aed2340e831574c41bc6acea753
Gabriel Azzinnari & Marcelo Coppola & Alejandro Aliaga <gazzinnari@humana.com>
2019-10-02 16:15:23 -0300

    Plantilla del ejercicio

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// En esta variable guardamos los resultados de la llamada
+const URL = "https://jsonplaceholder.typicode.com/users"
+var personas = []
+
+// En esta funcion creamos la logica para guardar los datos de la llamada y 
+// desplegar la info en el HTML
+function guardarYMostrarInfo(arreglo) {
+
+}
+
+// Dado el arreglo de personas despliega los resultados en nuestro HTML
+function mostrarInfo(arreglo) {
+
+}
+
+// Esta funcion nos ordena el arreglo de personas mediante el nombre
+// Luego lo vuelve a mostrar
+function ordenarPorNombre() {
+
+}
+
+// Esta funcion nos ordena el arreglo de personas mediante el ID
+// Luego lo vuelve a mostrar
+function ordenarPorId() {
+
+}
+
+// En esta funcion hacemos la logica para traer los resultados de nuestra URL a traves
+// mediante fetch
+function traerInfo(){
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    traerInfo()
+})
